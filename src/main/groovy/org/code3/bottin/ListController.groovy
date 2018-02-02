21d79649865a09c0ffe9663fa77fe285395ace06
Benjamin Dreux <benjamin.dreux@code3.ca>
2018-02-02 16:58:49 -0500

    add extraction of contact list

--- src/main/groovy/org/code3/bottin/ListController.groovy
+++ src/main/groovy/org/code3/bottin/ListController.groovy
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.servlet.mvc.support.RedirectAttributes
+import org.apache.poi.hssf.usermodel.HSSFWorkbook
+import javax.servlet.http.HttpServletResponse
 
 import org.code3.bottin.repository.ListRepository
 import org.code3.bottin.repository.ContactRepository
@@ -85,11 +87,61 @@ class ListController {
     "redirect:/lists/list"
   }
 
-  @GetMapping("/lists/{listId}/delete")
+  @GetMapping("/lists/{listId}/restore")
   def list_restore(@PathVariable Long listId, RedirectAttributes redirectAttributes){
     listRepository.archiveList(listId)
     redirectAttributes.addAttribute("restoredList", listId)
     "redirect:/lists/list"
+  }
+
+  @GetMapping("/lists/{listId}/extract")
+  def list_extract(@PathVariable long listId, HttpServletResponse response){
+    def conf = [
+      [header: "Firstname", getter: {contact -> contact.firstname}],
+      [header: "Lastname", getter: {contact -> contact.lastname}],
+      [header: "Organization", getter: {contact -> contact.organization_name}],
+      [header: "Email", getter: {contact -> contact?.emails ? contact?.emails[0]?.address : ""  }],
+    ]
+    def list = listRepository.getList(listId)
+    def members = list.members.collect({contactRepository.getContact(it)})
+
+
+    response.setHeader("Content-disposition", "attachment; filename=extract.xls")
+    response.setContentType("application/vnd.ms-excel")
+
+    def out = response.outputStream
+    exportMembersToExcelFile(conf, members, out)
+    out.flush()
+    out.close()
+
+  }
+
+  private def exportMembersToExcelFile(conf, members, out){
+    def wb = new HSSFWorkbook()
+    def rowIndex = 0
+    def columnIndex = 0
+
+    def sheet = wb.createSheet("Extract")
+    def row = sheet.createRow(rowIndex)
+
+    conf.each { col ->
+      def cell = row.createCell(columnIndex)
+      cell.setCellValue(col.header)
+      columnIndex++
+    }
+
+    members.each { member ->
+      rowIndex++
+      columnIndex = 0
+      row = sheet.createRow(rowIndex)
+      conf.each { col ->
+        def cell = row.createCell(columnIndex)
+        cell.setCellValue(col.getter(member))
+        columnIndex++
+      }
+
+    }
+    wb.write(out)
   }
 
 
